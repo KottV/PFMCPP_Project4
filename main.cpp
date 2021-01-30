087240cd76cfb8aaa7d5d3a4b4682990b99ecada
KottV <kill_it@mail.ru>
2021-01-30 10:36:43 +1000

    fix float and double compare fix formatting

--- main.cpp
+++ main.cpp
@@ -133,8 +133,8 @@ float FloatType::multiply( float lhs, float rhs )
 
 float FloatType::divide( float lhs, float rhs )
 {
-    if ( rhs == 0 ) std::cout<<std::endl<<"warning, floating point division by zero returns 'inf' !"<< std::endl;
-    return lhs / rhs;
+    if ( rhs == 0.0f ) std::cout << std::endl << "warning, floating point division by zero returns 'inf' !" << std::endl;
+        return lhs / rhs;
 }
 
 double DoubleType::add( double lhs, double rhs )
@@ -154,8 +154,8 @@ double DoubleType::multiply( double lhs, double rhs )
 
 double DoubleType::divide( double lhs, double rhs )
 {
-    if ( rhs == 0 ) std::cout<<std::endl<< "warning, floating point division by zero returns 'inf' !"<< std::endl;
-    return lhs / rhs;
+    if ( rhs == 0.0 ) std::cout << std::endl << "warning, floating point division by zero returns 'inf' !" << std::endl;
+        return lhs / rhs;
 }
 
 int IntType::add( int lhs, int rhs )
@@ -177,8 +177,7 @@ int IntType::divide( int lhs, int rhs )
 {
     if ( rhs == 0 )
     {
-        std::cout << "error, integer division by zero will crash the program!"<< std::endl;
-        std::cout<<"returning lhs"<< std::endl;
+        std::cout << "error, integer division by zero will crash the program!" << std::endl << "returning lhs" << std::endl;
         return lhs;
     }
     
